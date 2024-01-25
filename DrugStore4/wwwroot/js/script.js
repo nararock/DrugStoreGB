62e93c6080a0e4a407e2cfeb1ebc51c69417f9fd
Elena Gerasimovich <nararock@mail.ru>
2024-01-26 00:06:10 +0300

    Добавлены файлы со стилями, с шрифтами,, картинки, js библиотеки.

---
+++ DrugStore4/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+function createUser() {
+    user = {};
+    user.Name = document.querySelector('input[name="Name"]');
+    user.Email = document.querySelector('input[name="Email"]');
+    user.Password = document.querySelector('input[name="Password"]');
+}
