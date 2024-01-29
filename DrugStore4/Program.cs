1537dd88d4eeedde275b4af722f6cf431506136d
Elena Gerasimovich <nararock@mail.ru>
2024-01-29 16:32:30 +0300

    1.Добавлены Сервисы для подключения к б.д., Identity Framework, Entity Framework (в Program.cs). 2.Прописана строка подключения к б.д. (appsettings.json). 3.Уставолены соответствующие библиотеки.

--- DrugStore4/Program.cs
+++ DrugStore4/Program.cs
@@ -1,6 +1,33 @@
+using DrugStore4.DrugStoreDb;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DrugStoreDbContext>(options => options.UseSqlServer(connection));
+
+
+builder.Services.AddIdentity<DrugStore4.DrugStoreDb.User, IdentityRole>
+    (options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<DrugStoreDbContext>();
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(o =>
+                {
+                    o.LoginPath = new Microsoft.AspNetCore.Http.PathString("/login");
+                });
 
 var app = builder.Build();
 
