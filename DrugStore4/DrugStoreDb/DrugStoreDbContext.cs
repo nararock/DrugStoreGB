e9181ab382ff0d47ef083cc384f13106c3dfcc7b
Elena Gerasimovich <nararock@mail.ru>
2024-01-29 16:38:19 +0300

    1.В проект добавлена папка DrugStoreDb. Созданы классы DrugStoreDbContext и User.

---
+++ DrugStore4/DrugStoreDb/DrugStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DrugStore4.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrugStore4.DrugStoreDb
+{
+    public class DrugStoreDbContext: IdentityDbContext<User>
+    {
+        public DrugStoreDbContext(DbContextOptions<DrugStoreDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
---
+++ DrugStore4/DrugStoreDb/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DrugStore4.DrugStoreDb
+{
+    public class User: IdentityUser
+    {
+        public string? Name { get; set; }
+    }
+}
