b6a3c9ab83d0851a32b3cdcb2983d6239dcd6e45
Elena Gerasimovich <nararock@mail.ru>
2024-02-06 23:25:54 +0300

    1. Создан AdController и соответствующая страница Create.cshtml для создания объявления с лекарством. 2. Добавлены стили для страницы.

---
+++ DrugStore4/Controllers/AdController.cs
@@ -0,0 +1,21 @@
+﻿using DrugStore4.Classes;
+using DrugStore4.DrugStoreDb;
+using DrugStore4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugStore4.Controllers
+{
+    public class AdController : Controller
+    {
+        private DrugStoreDbContext _dbContext;
+        public AdController(DrugStoreDbContext drugStoreDb) {
+            _dbContext = drugStoreDb;
+        }
+        public IActionResult Create()
+        {
+            AdHelper adHelper = new AdHelper();
+            CreateAdModel createAdModel = adHelper.getTypeCategoryDrug(_dbContext);
+            return View(createAdModel);
+        }
+    }
+}
