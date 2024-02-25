a6488509aa96db9e77767fbd79821fad4e762223
Elena Gerasimovich <nararock@mail.ru>
2024-02-25 22:30:17 +0300

    1. Внесены изменения на главную страницу Index.cshtml: изменена верстка для вывода последних предложений. Добавлен метод в кнопку Подробнее. 2. В экшен Index класса Homecontroller внесены изменения: добавлен код для получения последних предложений.

--- DrugStore4/Controllers/HomeController.cs
+++ DrugStore4/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DrugStore4.Classes;
+using DrugStore4.DrugStoreDb;
+using DrugStore4.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DrugStore4.Controllers
 {
     public class HomeController : Controller
     {
+        private DrugStoreDbContext _dbContext;
+        public HomeController(DrugStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            CatalogHelper catalogHelper = new CatalogHelper();
+            CommonCatalogModel commonModel = catalogHelper.getCommonCatalogModel(_dbContext, 1, "", "0", "");
+            return View(commonModel);
         }
     }
 }
